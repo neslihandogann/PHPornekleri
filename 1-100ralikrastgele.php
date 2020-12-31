c4a7d3585d257e283aafda4bff80b8f31aabf277
NESLİNeslihanDogan/NESLİ <75863951+Nesliii0v1@users.noreply.github.com>
2020-12-31 17:29:06 +0300

    Add files via upload

---
+++ 1-100ralikrastgele.php
@@ -0,0 +1,21 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Başlıksız Belge</title>
+</head>
+
+<body>
+<?php
+for($i=0;$i<=100;$i++)
+{
+	$dizi[$i]=rand(1,1000);
+	echo $dizi[$i]."","<br>";
+}
+?>
+
+
+
+
+</body>
+</html>
---
+++ diziler.php
@@ -0,0 +1,22 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Başlıksız Belge</title>
+</head>
+
+<body>
+<?php
+$isimler=["ahmet","mehmet","hasan","ali","osman"];
+$isimler[2]="murat";
+echo $isimler[0]."<br>";
+echo $isimler[2];
+
+
+?>
+
+
+
+
+</body>
+</html>
